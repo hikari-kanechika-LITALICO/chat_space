adebc9320a50c36e4506be11a358b1e4287cbe31
古屋日華莉 <h.4u17.cc@gmail.com>
2016-10-23 23:24:28 +0900

    plus js, css,helper

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,10 @@ Bundler.require(*Rails.groups)
 module ChatSpace
   class Application < Rails::Application
     config.generators do |g|
-        g.test_framework false
+      g.test_framework false
+      g.javascripts false
+      g.stylesheets false
+      g.helper false
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
