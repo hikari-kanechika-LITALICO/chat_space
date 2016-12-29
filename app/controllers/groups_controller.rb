2d056457813982a1a73f8e2256926b587853e420
古屋日華莉 <h.4u17.cc@gmail.com>
2016-12-29 11:13:15 +0900

    group_controller created instance received a parameter

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,19 @@
 class GroupsController < ApplicationController
   def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(create_params)
+    redirect_to root_path
   end
 
   def edit
   end
+
+  private
+  def create_params
+    params.require(:group).permit(:name)
+  end
 end
+
