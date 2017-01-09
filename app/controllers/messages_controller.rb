91227b737266569765d3be7576e098d4fd62d91c
古屋日華莉 <h.4u17.cc@gmail.com>
2017-01-10 00:13:49 +0900

    create message function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,23 @@
 class MessagesController < ApplicationController
+  before_action :group_params
   def index
     @groups = Group.order(created_at: :desc)
-    @group = Group.find(params[:group_id])
     @messages = Message.where(group_id: params[:group_id])
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+    @message.save
+    redirect_to group_messages_path(@group)
+  end
+
+  private
+  def group_params
+    @group = Group.find(params[:group_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:text).merge(user_id: current_user.id, group_id: @group.id)
   end
 end
