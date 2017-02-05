93b61976ccd66f6796db1c11a35c335a5c23daba
古屋日華莉 <h.4u17.cc@gmail.com>
2017-02-05 13:25:28 +0900

    delete login admin

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,9 +1,4 @@
 module ControllerMacros
-  def login_admin(admin)
-    @request.env["devise.mapping"] = Devise.mappings[:admin]
-    sign_in admin
-  end
-
   def login_user(user)
     @request.env["devise.mapping"] = Devise.mappings[:user]
     sign_in user
