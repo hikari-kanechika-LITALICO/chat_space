2219273f550c4b6fbcb76a26f955d78395819b96
古屋日華莉 <h.4u17.cc@gmail.com>
2017-05-12 00:46:17 +0900

    Add user validate name

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence: true, length: { maximum: 8 }
   has_many :messages
   has_many :groups, through: :group_users
   has_many :group_users
