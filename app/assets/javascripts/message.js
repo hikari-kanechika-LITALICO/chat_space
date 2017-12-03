e33754b89790ed9dd7ffd2fa63df8259aca31d37
古屋日華莉 <h.4u17.cc@gmail.com>
2017-12-03 23:37:51 +0900

    Image transmission function implementation

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,21 +1,21 @@
 $(function() {
   function buildHTML(data) {
     var message = data.message
-    if (message.taxt) {
+    if (message.text) {
       var text = "<p>" + message.text + "</p>";
     } else {
       var text = "";
     }
-    if (data.image) {
+    if (message.image) {
       var image = '<img src="' + message.image + '">';
     } else {
       var image = "";
     }
     var html = $(
-      "<li class= 'chat-message'>" + 
-      "<div class = 'chat-message__header'>" + 
-      "<p class = 'chat-message__name'>" + message.name + "</p>" +  
-      "<p class = 'chat-message__time'>" + message.created_at + "</p>" + 
+      "<li class= 'chat-message'>" +
+      "<div class = 'chat-message__header'>" +
+      "<p class = 'chat-message__name'>" + message.name + "</p>" +
+      "<p class = 'chat-message__time'>" + message.created_at + "</p>" +
       "<p class = 'chat-message__body'>" + text + image + "</p>" + "</div>" + "</li>");
     return html;
   };
@@ -24,16 +24,15 @@ $(function() {
     e.preventDefault();
     var textField = $('.js-form__text-field');
     var message = textField.val();
+    var fd = new FormData($(this).get(0));
     $.ajax({
       url: 'messages',
       type: 'POST',
       dataType: 'json',
-      data: {
-        message: {
-          text: message
-        }
-      },
-      dataType: 'json'
+      data: fd,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
     .done(function(data) {
       var html = buildHTML(data);
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,4 +1,4 @@
-class ImageUploader < CarrierWave::Uploader::Base 
+class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   storage :fog
 
