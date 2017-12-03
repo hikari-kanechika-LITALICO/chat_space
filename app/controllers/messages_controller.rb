6c237cba9837ad741a5cb2cc83e6e30167db1b73
古屋日華莉 <h.4u17.cc@gmail.com>
2017-12-03 23:14:13 +0900

    Summarized in json file

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,12 +10,7 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
     if @message.save
       respond_to do |format|
-        format.json { render json: {
-          text: @message.text,
-          image: @message.image.url,
-          created_at: @message.created_at,
-          name: @message.user.name }}
-        flash[:notice] = 'メッセージ送信できました'
+        format.json
       end
     else
       flash[:alert] = 'メッセージを入力してください'
@@ -32,35 +27,3 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:text, :image).merge(user_id: current_user.id)
   end
 end
-
-
-  # def create
-  #   @message = @group.messages.new(message_params)
-  #   if @message.save
-  #     respond_to do |format|
-  #       # format.json { render json: {
-  #       #    text: @message.text,
-  #       #    image: @message.image.url,
-  #       #    created_at: @message.created_at,
-  #       #    name: @message.user.name }}
-  #       format.json { render json: @message }
-  #       flash[:notice] = 'メッセージ送信できました'
-  #     end
-  #   else
-  #     flash[:alert] = 'メッセージを入力してください'
-  #     render :index
-  #   end
-  #   # if @message.save
-  #   #   flash[:notice] = 'メッセージ送信できました'
-  #   #   respond_to do |format|
-  #   #     format.json { render json: {
-  #   #        text: @message.text,
-  #   #        image: @message.image.url,
-  #   #        created_at: @message.created_at,
-  #   #        name: @message.user.name }}
-  #   #   end
-  #   # else
-  #   #   flash[:alert] = 'メッセージを入力してください'
-  #   #   render :index
-  #   # end
-  # end
