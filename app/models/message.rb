3f5e051ff0c0171fa6292c929b46313966a260be
古屋日華莉 <h.4u17.cc@gmail.com>
2017-12-03 23:44:56 +0900

    Edit validation to image and text

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,10 @@
 class Message < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
-  validates :text, presence: true
+  validate :avoid_empty_message
   mount_uploader :image, ImageUploader
+
+  def avoid_empty_message
+    errors.add(:text, "を入力してください") unless text && image
+  end
 end
